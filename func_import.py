a793c9ab653aab59b63f64526c4157c570d110f3
LuAliSora <LuJYhF7rain02@outlook.com>
2024-10-12 07:56:21 +0800

    pathlib

--- func_import.py
+++ func_import.py
@@ -1,15 +1,30 @@
-import os
-import sys
-
-dir0=os.path.dirname(__file__)
-dir_func=os.path.join(dir0,'funcAll')
-# print("dir_func:",dir_func)
-sys.path.append(dir_func)
-
 from funcAll import *
 
-# import types
-# # 列出已导入的模块
-# imported_modules = [name for name, obj in globals().items() if isinstance(obj, types.ModuleType)]
-# for module in imported_modules:
-#     print(module)
+
+
+from pathlib import Path
+import sys
+
+partL=['funcAll']
+
+def addPart(partList)->None:
+    tempDir=Path.cwd()
+    while 1:
+        if tempDir.name=='crawler_firstTry':
+            break
+        if tempDir==tempDir.parent:
+            raise Exception("root_path: Lose!")
+        tempDir=tempDir.parent
+    # print("root_path:",tempDir)
+    [sys.path.append(str(tempDir/part)) for part in partList]
+
+addPart(partL)
+
+if __name__=="__main__":
+    # addPart(partL)
+    print(sys.path)
+    # import types
+    # # 列出已导入的模块
+    # imported_modules = [name for name, obj in globals().items() if isinstance(obj, types.ModuleType)]
+    # for module in imported_modules:
+    #     print(module)
